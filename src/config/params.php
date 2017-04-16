0f5dba4f856d836b8e798f8c6fb6ed91fff8377b
Andrii Vasyliev <sol@hiqdev.com>
2017-04-16 15:57:30 +0000

    added app.id and app.name params

--- src/config/params.php
+++ src/config/params.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'app.id'      => 'hisite',
+    'app.name'    => 'HiSite',
+
     'cookieValidationKey' => 'w42sd23fdsvsew234',
-    'debug_allowed_ips'   => [],
+
+    'debug_allowed_ips'   => ['127.0.0.1'],
 ];
