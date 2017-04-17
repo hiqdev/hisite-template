398296987c78bd238e33bf77e11807c27899c25e
Andrii Vasyliev <sol@hiqdev.com>
2017-04-17 09:58:27 +0000

    added use of params-local.php

--- src/config/params.php
+++ src/config/params.php
@@ -4,9 +4,10 @@ return [
     'app.id'      => 'hisite',
     'app.name'    => 'HiSite',
 
-    'cookieValidationKey' => 'w42sd23fdsvsew234',
+    'cookieValidationKey'       => 'w42sd23fdsvsew234',
 
-    'debug.allowedIps'   => ['127.0.0.1'],
+    'debug.enabled'             => true,
+    'debug.allowedIps'          => ['127.0.0.1'],
 
     'themeManager.defaultTheme' => 'original',
 ];
